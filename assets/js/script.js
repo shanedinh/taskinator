f46aef83ea50ed90ff7586b4b6df0aa6c845ed4c
Shane Dinh <shane.dinh5@gmail.com>
2022-05-23 18:31:25 -0500

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
-  
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
